4dc5e3c0f150ef7581aafa2a89b20314dee58139
tim-fan <tim.fanselow@gmail.com>
2015-09-20 21:03:56 +1200

    ExData: Course project 2, plots 1:4

--- exploratoryDataAnalysis/courseProject2/getProjectData.R
+++ exploratoryDataAnalysis/courseProject2/getProjectData.R
@@ -12,3 +12,57 @@ getProjectData <- function(){
   }
   d <- list(class = readRDS(dataClassificationFile), summary = readRDS(dataSummaryFile))
 }
+
+if (!exists('d')){
+  d <- getProjectData()
+}
+
+library("ggplot2")
+library("dplyr")
+# 
+# #plot 1
+# totalEmissions  <- 
+#   d$summary %>%
+#   group_by(year) %>%
+#   summarise(aggregatedEmissions = sum(Emissions))
+# 
+# with(totalEmissions, plot(year, aggregatedEmissions, type = 'l'))
+# 
+# #plot2
+# baltimoreTotalEmissions <-  
+#   d$summary %>%
+#   filter(fips == "24510") %>% 
+#   group_by(year) %>% 
+#   summarise(aggregatedEmissions = sum(Emissions))
+# 
+# with(baltimoreTotalEmissions, plot(year, aggregatedEmissions, type = 'l'))
+# 
+# #plot3
+# prcntOfFirstEl <- function(x) {x / first(x) * 100}
+# 
+# aggregatesByType <- 
+#   d$summary %>%
+#   group_by(year, type) %>% 
+#   summarise(aggregatedEmissions = sum(Emissions)) %>%
+#   group_by(type) %>% 
+#   mutate(aggregatedEmissionPercentage = prcntOfFirstEl(aggregatedEmissions))
+# 
+# qplot(year, aggregatedEmissionPercentage, data = aggregatesByType, color = type, geom = "line")
+# 
+
+#plot 4
+# coalCombustionSCCs <- 
+#   d$class %>%
+#   filter(grepl('combustion', SCC.Level.Four, ignore.case = TRUE) &&
+#          grepl('coal',       SCC.Level.Four, ignore.case = TRUE)) %>%
+#   select(SCC)
+
+elementsContainString <- function(string) {function(elements) {any(grepl(paste0('\\b',string,'\\b'),elements, ignore.case = TRUE))}}
+containsCoal <- elementsContainString('coal')
+containsCombustion <- elementsContainString('combustion')
+coalCombustionSCCs <- 
+  d$class %>%
+  select(-Short.Name, -EI.Sector) %>% #remove these columns from the following grep - they contain 'or' values
+  filter(apply(., 1, containsCoal), apply(., 1, containsCombustion)) %>% 
+  select(SCC) 
+
