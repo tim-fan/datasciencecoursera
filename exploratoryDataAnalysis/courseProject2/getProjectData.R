5553f8db24fc0e47431670c4f9102f4c97e46f3c
Tim Fanselow <tim.fanselow@crown.com>
2015-09-28 10:12:36 +1300

    Course proj 2, minor cleanups/corrections

--- exploratoryDataAnalysis/courseProject2/getProjectData.R
+++ exploratoryDataAnalysis/courseProject2/getProjectData.R
@@ -17,74 +17,137 @@ if (!exists('d')){
   d <- getProjectData()
 }
 
-library("ggplot2")
+
+
+
+#plot 1
 library("dplyr")
-# 
-# #plot 1
-# totalEmissions  <- 
-#   d$summary %>%
-#   group_by(year) %>%
-#   summarise(aggregatedEmissions = sum(Emissions))
-# 
-# with(totalEmissions, plot(year, aggregatedEmissions, type = 'l'))
-# 
-# #plot2
-# baltimoreTotalEmissions <-  
-#   d$summary %>%
-#   filter(fips == "24510") %>% 
-#   group_by(year) %>% 
-#   summarise(aggregatedEmissions = sum(Emissions))
-# 
-# with(baltimoreTotalEmissions, plot(year, aggregatedEmissions, type = 'l'))
-# 
-# #plot3
-# prcntOfFirstEl <- function(x) {x / first(x) * 100}
-# 
-# aggregatesByType <- 
-#   d$summary %>%
-#   group_by(year, type) %>% 
-#   summarise(aggregatedEmissions = sum(Emissions)) %>%
-#   group_by(type) %>% 
-#   mutate(aggregatedEmissionPercentage = prcntOfFirstEl(aggregatedEmissions))
-# 
-# qplot(year, aggregatedEmissionPercentage, data = aggregatesByType, color = type, geom = "line")
-# 
-# 
-# #plot 4
-# 
-# #find SCC codes related to coal combustion.
-# #To do this, just grep for rows where the word 'coal' AND 'combustion' appear.
-# #However, exclude columns 'Short.Name' and 'EI.Sector' from the grep, to avoid categories 
-# #like 'Ext Comb /Electric Gen /Anthracite Coal /Pulverized Coal', which appear to include coal
-# #as well as non-coal related sources (too general!)
-# 
-# #make functions for grepping for 'coal' and 'combustion'
-# elementsContainString <- function(string) {function(elements) {any(grepl(paste0('\\b',string,'\\b'),elements, ignore.case = TRUE))}}
-# containsCoal <- elementsContainString('coal')
-# containsCombustion <- elementsContainString('combustion')
-# 
-# #do the grep and extract the remaining SCC codes
-# coalCombustionClasses <- 
-#   d$class %>%
-#   select(-Short.Name, -EI.Sector) %>% #remove these columns from the following grep - they contain 'or' values
-#   filter(apply(., 1, containsCoal), apply(., 1, containsCombustion)) %>% 
-#   select(SCC)
-# coalCombustionSCCs <- coalCombustionClasses$SCC
-# 
-# #now we know which codes relate to coal combustion. Filter the summary data
-# #for coal-commbustion related data
-# coalCombustionData <- 
-#   d$summary %>%
-#   filter(SCC %in% coalCombustionSCCs) %>%
-#   group_by(year) %>%
-#   summarise(aggregatedEmissions = sum(Emissions))
-# 
-# #now we can plot the trend for aggregate coal combustion related emissions
-# qplot(year, aggregatedEmissions, data = coalCombustionData, geom = 'line')
-# 
+
+#aggregate emissions over years
+totalEmissions  <- 
+  d$summary %>%
+  group_by(year) %>%
+  summarise(aggregatedEmissions = sum(Emissions))
+
+#open the png device
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+#create plot
+with(totalEmissions, plot(year, aggregatedEmissions, type = 'l'))
+
+#close the png device
+dev.off()
+
+
+#plot2
+library("dplyr")
+
+#filter for Baltimore, and aggregate emissions over years
+baltimoreTotalEmissions <-  
+  d$summary %>%
+  filter(fips == "24510") %>% 
+  group_by(year) %>% 
+  summarise(aggregatedEmissions = sum(Emissions))
+
+#open the png device
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+
+#create plot
+with(baltimoreTotalEmissions, plot(year, aggregatedEmissions, type = 'l'))
+
+#close the png device
+dev.off()
+
+
+
+
+#plot3
+library("dplyr")
+library("ggplot2")
+
+#helper function: converts vector of values
+#to vector of percentages of first value
+prcntOfFirstEl <- function(x) {x / first(x) * 100}
+
+#filter for Baltimore, aggregate emissions over years for each type,
+#and convert absolute emissions to percentage of 2009 levels
+aggregatesByType <- 
+  d$summary %>%
+  group_by(year, type) %>% 
+  summarise(aggregatedEmissions = sum(Emissions)) %>%
+  group_by(type) %>% 
+  mutate(aggregatedEmissionPercentage = prcntOfFirstEl(aggregatedEmissions))
+
+#open the png device
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+
+#create plot
+qplot(
+        x = year, 
+        y = aggregatedEmissionPercentage, 
+        data = aggregatesByType, 
+        color = type, 
+        geom = "line",
+        ylim = c(0,120),
+        ylab = "Aggrregated Emissions\nPercentage of 1999 levels"
+      )
+
+#close the png device
+dev.off()
+
+
+#plot 4
+library("dplyr")
+library("ggplot2")
+
+#find SCC codes related to coal combustion.
+#To do this, just grep for rows where the word 'coal' AND 'combustion' appear.
+#However, exclude columns 'Short.Name' and 'EI.Sector' from the grep, to avoid categories 
+#like 'Ext Comb /Electric Gen /Anthracite Coal /Pulverized Coal', which appear to include coal
+#as well as non-coal related sources (too general!)
+
+#make functions for grepping for 'coal' and 'combustion'
+elementsContainString <- function(string) {function(elements) {any(grepl(paste0('\\b',string,'\\b'),elements, ignore.case = TRUE))}}
+containsCoal <- elementsContainString('coal')
+containsCombustion <- elementsContainString('combustion')
+
+#do the grep and extract the remaining SCC codes
+coalCombustionClasses <- 
+  d$class %>%
+  select(-Short.Name, -EI.Sector) %>% #remove these columns from the following grep - they contain 'or' values
+  filter(apply(., 1, containsCoal), apply(., 1, containsCombustion)) %>% 
+  select(SCC)
+coalCombustionSCCs <- coalCombustionClasses$SCC
+
+#now we know which codes relate to coal combustion. Filter the summary data
+#for coal-combustion related data, and aggregate over years
+coalCombustionData <- 
+  d$summary %>%
+  filter(SCC %in% coalCombustionSCCs) %>%
+  group_by(year) %>%
+  summarise(aggregatedEmissions = sum(Emissions))
+
+#now we can plot the trend for aggregate coal combustion related emissions
+
+#open the png device
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+
+#create plot
+qplot(
+        x = year, 
+        y = aggregatedEmissions, 
+        data = coalCombustionData, 
+        geom = 'line'
+      )
+
+#close the png device
+dev.off()
+
 
 
 # Plot 5:
+library("dplyr")
+library("ggplot2")
 
 # Notes on determining 'motor vehicle' SCC codes:
 #
@@ -133,6 +196,10 @@ baltimoreMobVehicleTotalEmissions <-
   group_by(year, EI.Sector) %>% 
   summarise(aggregatedEmissions = sum(Emissions))
 
+#open the png device
+png(filename = "plot5.png", width = 700, height = 480, units = "px")
+
+#create plot
 qplot(
         x = year, 
         y = aggregatedEmissions, 
@@ -141,7 +208,14 @@ qplot(
         fill = EI.Sector
       )
 
+#close the png device
+dev.off()
+
+
+
 #plot6:
+library("dplyr")
+library("ggplot2")
 library(gridExtra)
 
 #same logic from plot 5 for choosing mobile vehicle SCC codes
@@ -151,9 +225,18 @@ mobileVehicleClasses <-
 mobileVehicleSccs <- unique(mobileVehicleClasses$SCC)
 
 #In absolute terms the changes in Los Angeles County emissions are much greater
-#than those for Baltimore. 
+#than those for Baltimore. However in terms of percentage changes from 2009 levels,
+#Baltimore city has seen greater changes.
+#I will plot both absolute and relative levels, and let the reader decide which is more
+#useful.
+
+#helper functions
 prcntOfFirstEl <- function(x) {x / first(x) * 100}
 fipsToString <- function(fips) {if (fips == "06037") "Los Angeles County" else "Baltimore City"}
+
+#filter for the two counties in question, aggregate emissions over years,
+#add a column for percentage change since 2009, and add a column for
+#county name
 baltimoreLATotalEmissions <-  
   d$summary %>%
   filter(fips %in% c("24510", "06037"), SCC %in% mobileVehicleSccs) %>% 
@@ -164,6 +247,10 @@ baltimoreLATotalEmissions <-
   rowwise() %>%
   mutate(County = fipsToString(fips))
 
+#open the png device
+png(filename = "plot6.png", width = 480, height = 480, units = "px")
+
+#create plots
 p1 <- qplot(
   x = year, 
   y = aggregatedEmissions, 
@@ -183,3 +270,6 @@ p2 <- qplot(
   ylab = "Aggrregated Emissions\nPercentage of 1999 levels"
 )
 grid.arrange(p1,p2)
+
+#close the png device
+dev.off()
