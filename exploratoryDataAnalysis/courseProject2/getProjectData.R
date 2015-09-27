23b3044555b5c27765e4d6d197a3069c6c132d41
tim-fan <tim.fanselow@gmail.com>
2015-09-27 17:22:49 +1300

    Course proj 2, plot 6

--- exploratoryDataAnalysis/courseProject2/getProjectData.R
+++ exploratoryDataAnalysis/courseProject2/getProjectData.R
@@ -4,7 +4,7 @@ getProjectData <- function(){
   dataClassificationFile <- 'data/Source_Classification_Code.rds'
   dataSummaryFile <- 'data/summarySCC_PM25.rds'
   
-  #download file if necesarry
+  #download file if necessary
   dir.create("data", showWarnings = FALSE)
   if (!file.exists(dataClassificationFile) || !file.exists(dataSummaryFile)){
     download.file(url = dataUrl, destfile = zippedFile) #
@@ -49,35 +49,118 @@ library("dplyr")
 # 
 # qplot(year, aggregatedEmissionPercentage, data = aggregatesByType, color = type, geom = "line")
 # 
+# 
+# #plot 4
+# 
+# #find SCC codes related to coal combustion.
+# #To do this, just grep for rows where the word 'coal' AND 'combustion' appear.
+# #However, exclude columns 'Short.Name' and 'EI.Sector' from the grep, to avoid categories 
+# #like 'Ext Comb /Electric Gen /Anthracite Coal /Pulverized Coal', which appear to include coal
+# #as well as non-coal related sources (too general!)
+# 
+# #make functions for grepping for 'coal' and 'combustion'
+# elementsContainString <- function(string) {function(elements) {any(grepl(paste0('\\b',string,'\\b'),elements, ignore.case = TRUE))}}
+# containsCoal <- elementsContainString('coal')
+# containsCombustion <- elementsContainString('combustion')
+# 
+# #do the grep and extract the remaining SCC codes
+# coalCombustionClasses <- 
+#   d$class %>%
+#   select(-Short.Name, -EI.Sector) %>% #remove these columns from the following grep - they contain 'or' values
+#   filter(apply(., 1, containsCoal), apply(., 1, containsCombustion)) %>% 
+#   select(SCC)
+# coalCombustionSCCs <- coalCombustionClasses$SCC
+# 
+# #now we know which codes relate to coal combustion. Filter the summary data
+# #for coal-commbustion related data
+# coalCombustionData <- 
+#   d$summary %>%
+#   filter(SCC %in% coalCombustionSCCs) %>%
+#   group_by(year) %>%
+#   summarise(aggregatedEmissions = sum(Emissions))
+# 
+# #now we can plot the trend for aggregate coal combustion related emissions
+# qplot(year, aggregatedEmissions, data = coalCombustionData, geom = 'line')
+# 
 
-#plot 4
 
-#find SCC codes related to coal combustion.
-#To do this, just grep for rows where the word 'coal' AND 'combustion' appear.
-#However, exclude columns 'Short.Name' and 'EI.Sector' from the grep, to avoid categories 
-#like 'Ext Comb /Electric Gen /Anthracite Coal /Pulverized Coal', which appear to include coal
-#as well as non-coal related sources (too general!)
+# Plot 5:
 
-#make functions for grepping for 'coal' and 'combustion'
-elementsContainString <- function(string) {function(elements) {any(grepl(paste0('\\b',string,'\\b'),elements, ignore.case = TRUE))}}
-containsCoal <- elementsContainString('coal')
-containsCombustion <- elementsContainString('combustion')
+# Notes on determining 'motor vehicle' SCC codes:
+#
+# See below for EI.Sector values mobile sources:
+# levels(d$class$EI.Sector)
+# [43] "Mobile - Aircraft"                                 
+# [44] "Mobile - Commercial Marine Vessels"                
+# [45] "Mobile - Locomotives"                              
+# [46] "Mobile - Non-Road Equipment - Diesel"              
+# [47] "Mobile - Non-Road Equipment - Gasoline"            
+# [48] "Mobile - Non-Road Equipment - Other"               
+# [49] "Mobile - On-Road Diesel Heavy Duty Vehicles"       
+# [50] "Mobile - On-Road Diesel Light Duty Vehicles"       
+# [51] "Mobile - On-Road Gasoline Heavy Duty Vehicles"     
+# [52] "Mobile - On-Road Gasoline Light Duty Vehicles"
+#
+# From these, I decided to exclude all 'equipment' as a type of vehicle,
+# and to include trains, planes and boats as motorised vehicles (beacuse these are all types of vehicles with a motor)
+# Note that the effects of including/excluding planes, trains and equipment is pretty small,
+# but the effect of including boats is significant. I've color coded my plot to make the 
+# contributions from various sources clear.
 
-#do the grep and extract the remaining SCC codes
-coalCombustionClasses <- 
-  d$class %>%
-  select(-Short.Name, -EI.Sector) %>% #remove these columns from the following grep - they contain 'or' values
-  filter(apply(., 1, containsCoal), apply(., 1, containsCombustion)) %>% 
-  select(SCC)
-coalCombustionSCCs <- coalCombustionClasses$SCC
+mobileVehicleClasses <- 
+    d$class %>% 
+    filter(grepl('Mobile', EI.Sector) & !grepl('Equipment',EI.Sector))
 
-#now we know which codes relate to coal combustion. Filter the summary data
-#for coal-commbustion related data
-coalCombustionData <- 
+# Included EI.Sectors:
+# > unique(mobileVehicleClasses$EI.Sector)
+# [1] Mobile - On-Road Gasoline Light Duty Vehicles
+# [2] Mobile - On-Road Gasoline Heavy Duty Vehicles
+# [3] Mobile - On-Road Diesel Light Duty Vehicles  
+# [4] Mobile - On-Road Diesel Heavy Duty Vehicles  
+# [5] Mobile - Aircraft                            
+# [6] Mobile - Commercial Marine Vessels           
+# [7] Mobile - Locomotives           
+
+mobileVehicleSccs <- unique(mobileVehicleClasses$SCC)
+
+#now we have the relevant SCCs, filter the data to get 
+#Baltimore motor-vehicle emissions by year
+
+baltimoreMobVehicleTotalEmissions <- 
   d$summary %>%
-  filter(SCC %in% coalCombustionSCCs) %>%
-  group_by(year) %>%
+  filter(fips == "24510", SCC %in% mobileVehicleSccs) %>% 
+  left_join(d$class) %>%    #join to get EI.Sector codes, for color coding
+  group_by(year, EI.Sector) %>% 
   summarise(aggregatedEmissions = sum(Emissions))
 
-#now we can plot the trend for aggregate coal combustion related emissions
-qplot(year, aggregatedEmissions, data = coalCombustionData, geom = 'line')
+qplot(
+        x = year, 
+        y = aggregatedEmissions, 
+        data = baltimoreMobVehicleTotalEmissions, 
+        geom = 'area', 
+        fill = EI.Sector
+      )
+
+#plot6:
+mobileVehicleClasses <- 
+  d$class %>% 
+  filter(grepl('Mobile', EI.Sector) & !grepl('Equipment',EI.Sector))
+mobileVehicleSccs <- unique(mobileVehicleClasses$SCC)
+
+prcntOfFirstEl <- function(x) {x / first(x) * 100}
+
+baltimoreLATotalEmissions <-  
+  d$summary %>%
+  filter(fips %in% c("24510", "06037"), SCC %in% mobileVehicleSccs) %>% 
+  group_by(year, fips) %>% 
+  summarise(aggregatedEmissions = sum(Emissions)) %>%
+  group_by(fips) %>%
+  mutate(aggregatedEmissionPercentage = prcntOfFirstEl(aggregatedEmissions ))
+
+qplot(
+        x = year, 
+        y = aggregatedEmissionPercentage, 
+        data = baltimoreLATotalEmissions, 
+        geom = 'line', 
+        color = fips
+      )
